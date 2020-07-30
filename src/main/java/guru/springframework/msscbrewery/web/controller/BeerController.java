6002e6c101ef4291b32fe292be58c38da700f733
FrankiPanki <puchicher@gmail.com>
2020-07-30 14:22:26 -0500

    Implementacion de controladores

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -1,5 +1,7 @@
 package guru.springframework.msscbrewery.web.controller;
 
+
+
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
 import org.springframework.http.HttpHeaders;
@@ -7,13 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.UUID;
 
-/**
- * Created by jt on 2019-04-20.
- */
-@Deprecated
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
@@ -24,36 +21,33 @@ public class BeerController {
         this.beerService = beerService;
     }
 
-    @GetMapping({"/{beerId}"})
+    //Si se llaman igual la variable del path y el parametro no es nescesario @PathVariable
+    @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
-
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+
     }
 
-    @PostMapping // POST - create new beer
-    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto){
-
-        BeerDto savedDto = beerService.saveNewBeer(beerDto);
-
+    //Indicamos en donde se a guanrdado el recurso
+    //@RequestBody es nescesario, ya que indicamos que el parametro va asociado al request del cliente
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+        BeerDto save = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
-        //todo add hostname to url
-        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
-
-        return new ResponseEntity(headers, HttpStatus.CREATED);
+        //todo añadir hostname al url
+        headers.add("Location","/api/v1/beer/"+save.getId().toString());
+        return new ResponseEntity(headers,HttpStatus.CREATED);
     }
 
-    @PutMapping({"/{beerId}"})
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto){
-
-        beerService.updateBeer(beerId, beerDto);
-
+    @PutMapping("/{beerId}")
+    public ResponseEntity handlePut(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        beerService.updateBeer(beerId,beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping({"/{beerId}"})
+    @DeleteMapping("/{beerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBeer(@PathVariable("beerId") UUID beerId){
+    public void handleDelete(@PathVariable("beerId") UUID beerId){
         beerService.deleteById(beerId);
     }
-
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -7,51 +7,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-/**
- * Created by jt on 2019-04-21.
- */
-
-@RequestMapping("api/v1/customer")
+@RequestMapping("/api/v1/customer")
 @RestController
 public class CustomerController {
 
-    private CustomerService customerService;
+    public final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
-    @GetMapping("/{customerId}")
-    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId")  UUID customerId){
-
-        return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    @GetMapping("/{id}")
+    ResponseEntity<CustomerDto> getById(@PathVariable(value = "id") UUID id){
+        return new ResponseEntity(customerService.getCustomerById(id), HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity handlePost(@Valid @RequestBody CustomerDto customerDto){
-        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
-
-        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
+    @PostMapping("/{id}")
+    ResponseEntity create(@RequestBody CustomerDto customerDto, @PathVariable(value = "id") UUID id ){
+        CustomerDto save = customerService.saveNewCustomer(customerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/customer/"+ save.getId().toString());
+        return new ResponseEntity(headers,HttpStatus.CREATED);
     }
 
-    @PutMapping("/{customerId}")
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handleUpdate(@PathVariable("customerId") UUID customerId, @Valid @RequestBody CustomerDto customerDto){
-        customerService.updateCustomer(customerId, customerDto);
+    void actualizar(@RequestBody CustomerDto customerDto, @PathVariable(value = "id") UUID id ){
+        customerService.updateCustomer(id,customerDto);
     }
 
-    @DeleteMapping("/{customerId}")
-    public void deleteById(@PathVariable("customerId")  UUID customerId){
-        customerService.deleteById(customerId);
-    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void borrar( @PathVariable(value = "id") UUID id){
+        customerService.deleteById(id);
 
+    }
 }
